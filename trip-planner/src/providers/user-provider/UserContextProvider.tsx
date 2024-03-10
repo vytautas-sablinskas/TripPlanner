7be971e49c882d7ac89ecc560f07bc90e50097bb
Vytautas Šablinskas <vytsab2@ktu.lt>
2024-03-10 14:23:47 +0200

    Remake trip planner base to vite

---
+++ trip-planner/src/providers/user-provider/UserContextProvider.tsx
@@ -0,0 +1,58 @@
+import { useState, useCallback, useMemo } from 'react';
+import UserContext from './UserContext';
+
+const UserContextProvider = ({ children }: any) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem('isAuthenticated') === 'true');
+    const [role, setRole] = useState(localStorage.getItem('role') || 'none');
+
+    function decodeJWT(token : string) {
+        try {
+            const base64Url = token.split('.')[1];
+            const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+            const payload = JSON.parse(atob(base64));
+            return payload;
+        } catch (error) {
+            console.error("Failed to decode JWT:", error);
+            return null;
+        }
+    }
+
+    const changeUserInformationToLoggedIn = useCallback((newAccessToken: string, newRefreshToken: string) => {
+        debugger;
+        const payload = decodeJWT(newAccessToken);
+        const userRole = payload ? payload['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'] : 'none';
+
+        setIsAuthenticated(true);
+        setRole(userRole);
+
+        localStorage.setItem('isAuthenticated', 'true');
+        localStorage.setItem('role', userRole);
+        localStorage.setItem('accessToken', newAccessToken);
+        localStorage.setItem('refreshToken', newRefreshToken);
+    }, []);
+
+    const changeUserInformationToLoggedOut = useCallback(() => {
+        setIsAuthenticated(false);
+        setRole('none');
+
+        localStorage.removeItem('isAuthenticated');
+        localStorage.removeItem('role');
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+    }, []);
+
+    const contextValue = useMemo(() => ({
+        isAuthenticated,
+        role,
+        changeUserInformationToLoggedIn,
+        changeUserInformationToLoggedOut
+    }), [isAuthenticated, role, changeUserInformationToLoggedIn, changeUserInformationToLoggedOut]);
+
+    return (
+        <UserContext.Provider value={contextValue}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+export default UserContextProvider;
---
+++ trip-planner/src/routes/AppRoutes.tsx
@@ -0,0 +1,26 @@
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import PageNotFound from "../pages/PageNotFound";
+import Register from "../pages/Register";
+
+const AppRoutes = [
+  {
+    index: true,
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '*',
+    element: <PageNotFound />
+  }
+];
+
+export default AppRoutes;
