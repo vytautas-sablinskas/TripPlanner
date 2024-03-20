b14c3072ff9dcbe99d11e50c94970a2e236aa695
vytcka <vytas.sa1@gmail.com>
2024-03-20 19:41:26 +0200

    Sort arrays by ascending time order

--- trip-planner/src/pages/TripDetails/TripDetailsAccordion.tsx
+++ trip-planner/src/pages/TripDetails/TripDetailsAccordion.tsx
@@ -9,15 +9,18 @@ import "./styles/trip-details-accordion.css";
 import { formatDateToString } from "@/utils/date";
 
 const TripDetailsAccordion = ({ tripDetails, onDelete }: any) => {
-  const days = Object.keys(tripDetails);
-
+  const days = Object.keys(tripDetails).sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+  const sortedDays: { [key: string]: any[] } = days.reduce((acc, day) => {
+    const sortedDetails = tripDetails[day].sort((a : any, b : any) => new Date(a.startTime).getTime() - new Date(b.startTime).getTime());
+    return { ...acc, [day]: sortedDetails };
+  }, {});
   return (
     <Accordion type="multiple" className="w-full">
       {days.map((day, index) => (
         <AccordionItem key={index} value={`item-${index + 1}`}>
           <AccordionTrigger>{formatDateToString(day)}</AccordionTrigger>
           <AccordionContent className="day-container">
-            {tripDetails[day].map((detail : any, detailIndex : any) => (
+            {sortedDays[day].map((detail : any, detailIndex : any) => (
               <TripDetailCard key={detailIndex} detail={detail} onDelete={onDelete}/>
             ))}
           </AccordionContent>
