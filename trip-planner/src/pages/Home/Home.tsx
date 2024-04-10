4130ccac969559391362c64796d40cb45db1ede6
vytcka <vytas.sa1@gmail.com>
2024-04-10 23:50:49 +0300

    Add component for autocompletion of address

--- trip-planner/src/pages/Home/Home.tsx
+++ trip-planner/src/pages/Home/Home.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import GoogleAutocomplete from "@/components/Extra/GoogleAutocomplete";
+import React, { useEffect } from "react";
+import Autocomplete from "react-google-autocomplete";
 
 const Home = () => {
-    return (<p>Home page</p>)
-}
+  const key = import.meta.env.VITE_GOOGLE_API_KEY;
+  console.log(key);
+
+  useEffect(() => {
+    console.log(import.meta.env.VITE_GOOGLE_API_KEY);
+  })
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <GoogleAutocomplete />
+    </div>
+  );
+};
 
 export default Home;
