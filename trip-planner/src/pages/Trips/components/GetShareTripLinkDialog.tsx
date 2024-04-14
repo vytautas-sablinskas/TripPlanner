9ab472a853343992f87ebe99f5259be2f1e13fb2
vytcka <vytas.sa1@gmail.com>
2024-04-14 23:30:24 +0300

    Fix google maps directions bug when no places are selected

--- trip-planner/src/pages/Trips/components/GetShareTripLinkDialog.tsx
+++ trip-planner/src/pages/Trips/components/GetShareTripLinkDialog.tsx
@@ -7,82 +7,112 @@ import { Label } from "@/components/ui/label";
 import { useUser } from "@/providers/user-provider/UserContext";
 import Paths from "@/routes/Paths";
 import { checkTokenValidity } from "@/utils/jwtUtils";
+import { CirclePlus, CircleX, Copy } from "lucide-react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-const GetShareTripLinkDialog = ({
-    open,
-    setOpen,
-    link,
-    setLink
-} : any) => {
-    const { changeUserInformationToLoggedIn, changeUserInformationToLoggedOut } = useUser();
-    const navigate = useNavigate();
-    const [isSubmitting, setIsSubmitting] = useState(false);
+const GetShareTripLinkDialog = ({ open, setOpen, link, setLink }: any) => {
+  const { changeUserInformationToLoggedIn, changeUserInformationToLoggedOut } =
+    useUser();
+  const navigate = useNavigate();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [state, setState] = useState<any>({
+    value: link ? link : "",
+    copied: false,
+  })
 
-    const getTripId = () => {
-        const pathParts = location.pathname.split("/");
-        return pathParts[pathParts.length - 2];
+  const getTripId = () => {
+    const pathParts = location.pathname.split("/");
+    return pathParts[pathParts.length - 2];
+  };
+
+  const updateLink = async () => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (!checkTokenValidity(accessToken || "")) {
+      const result = await refreshAccessToken();
+      if (!result.success) {
+        toast.error("Session has expired. Login again!", {
+          position: "top-center",
+        });
+
+        changeUserInformationToLoggedOut();
+        navigate(Paths.LOGIN);
+        return;
+      }
+
+      changeUserInformationToLoggedIn(
+        result.data.accessToken,
+        result.data.refreshToken,
+        result.data.id
+      );
     }
 
-    const updateLink = async () => {
-        const accessToken = localStorage.getItem("accessToken");
+    try {
+      setIsSubmitting(true);
+      const response = await updateShareTripLink(getTripId());
+      if (!response.ok) {
+        toast.error("Failed to update link. Try again later.", {
+          position: "top-center",
+        });
+        setIsSubmitting(false);
+        return;
+      }
 
-        if (!checkTokenValidity(accessToken || "")) {
-            const result = await refreshAccessToken();
-            if (!result.success) {
-            toast.error("Session has expired. Login again!", {
-                position: "top-center",
-            });
-
-            changeUserInformationToLoggedOut();
-            navigate(Paths.LOGIN);
-            return;
-            }
-
-            changeUserInformationToLoggedIn(
-            result.data.accessToken,
-            result.data.refreshToken,
-            result.data.id
-            );
-        }
-
-        try {
-            setIsSubmitting(true);
-            const response = await updateShareTripLink(getTripId());
-            if (!response.ok) {
-                toast.error("Failed to update link. Try again later.", {
-                    position: "top-center",
-                });
-                setIsSubmitting(false);
-                return;
-            }
-
-            const data = await response.json();
-            setLink(data?.link === null ? "" : data.link);
-            setIsSubmitting(false);
-        } catch {
-            toast.error("Failed to update link. Try again later.", {
-                position: "top-center",
-            });
-            setIsSubmitting(false);
-        }
+      const data = await response.json();
+      setLink(data?.link === null ? "" : data.link);
+      setIsSubmitting(false);
+    } catch {
+      toast.error("Failed to update link. Try again later.", {
+        position: "top-center",
+      });
+      setIsSubmitting(false);
     }
+  };
 
-    return (
-        <Dialog open={open} onOpenChange={() => setOpen(!open)}>
-            <DialogContent>
-                <Label>Link For Sharing Trip</Label>
-                <Input className="p-2" placeholder="To get link press generate link button" value={link}/>
-                <div className="w-full flex justify-end">
-                    <Button onClick={updateLink}>
-                        {link && link.length > 0 ? "Remove Link" : "Generate Link"}
-                    </Button>
-                </div>
-            </DialogContent>
-        </Dialog>
-    )
-}
+  const onCopy = () => {
+    setState({ ...state, copied: true });
+    toast.success("Link copied to clipboard", {
+      position: "top-center",
+    });
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={() => setOpen(!open)}>
+      <DialogContent>
+        <Label>Link For Sharing Trip</Label>
+        <Input
+          className="p-2"
+          placeholder="To get link press create link button"
+          value={link}
+          onChange={() => {}}
+        />
+        <div className="w-full flex justify-end">
+          <Button onClick={updateLink} className="mr-2">
+            {!link || link.length <= 0 ? (
+              <div className="flex items-center">
+                <CirclePlus className="mr-2 h-4 w-4" />
+                <p>Create Link</p>
+              </div>
+            ) : (
+              <div className="flex items-center">
+                <CircleX className="mr-2 h-4 w-4" />
+                <p>Remove Link</p>
+              </div>
+            )}
+          </Button>
+          <CopyToClipboard text={link} onCopy={() => onCopy()}>
+            <Button disabled={!link || link.length === 0}>
+                <Copy className="mr-2 h-4 w-4" />
+                Copy Link
+            </Button>
+          </CopyToClipboard>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default GetShareTripLinkDialog;
