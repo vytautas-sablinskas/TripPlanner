fb39ce3fb66ac24f692c7f4e543d900057822485
vytcka <vytas.sa1@gmail.com>
2024-04-16 22:00:14 +0300

    Add home page design

--- trip-planner/src/components/Shared/Layout.tsx
+++ trip-planner/src/components/Shared/Layout.tsx
@@ -2,15 +2,28 @@ import Footer from "./Footer";
 import Header from "./Header";
 import { Toaster } from "sonner";
 import "./styles/layout.css";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Layout = ({ children }: any) => {
+  const location = useLocation();
+  const [homePage, setHomePage] = useState(false);
+
+  useEffect(() => {
+    setHomePage(location.pathname === "/")
+  }, [location.pathname])
+
   return (
-    <div className="main-body">
-      <Header />
-      <main style={{ flex: 1, display: "flex", width: "100%" }}>
+    <div className={`${homePage ? "main-body-full homepage-background" : "main-body"}`}>
+      <div className="w-full flex justify-center">
+        <Header isHomePage={homePage}/>
+      </div>
+      <main style={{ flex: 1, display: "flex", width: homePage ? "1110px" : "100%", justifyContent: homePage ? "center" : "normal" }}>
         {children}
       </main>
-      <Footer />
+      {!homePage && (
+        <Footer />
+      )}
       <Toaster richColors closeButton />
     </div>
   );
