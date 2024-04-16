7df5864b830d8f9e94883bbd5fcb847701f8ce51
vytcka <vytas.sa1@gmail.com>
2024-04-16 22:09:29 +0300

    Fix responsiveness of text in home page

--- trip-planner/src/pages/Home/Home.tsx
+++ trip-planner/src/pages/Home/Home.tsx
@@ -19,9 +19,10 @@ const Home = () => {
     <div
       className="h-screen flex justify-center items-center"
     >
-      <div className="text-center">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl text-white font-bold mb-6">
-          Embark on Your Journey: Capture, Share, Explore!
+      <div className="text-center" style={{ wordBreak: "break-all" }}>
+      <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl text-white font-bold mb-6">
+          Embark on Your Journey: <br /> Capture, Share,
+          Explore!
         </h1>
         <button className="bg-blue-500 hover:bg-blue-600 text-white py-3 px-8 rounded-full shadow-lg text-lg font-semibold transition duration-300" onClick={() => onExploreClick()}>
           Start Exploring
